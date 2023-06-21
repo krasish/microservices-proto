e079be7d3ebf69382b5f7e1b1a06b2d8b595649e
krasish <krasishontov@gmail.com>
2023-06-21 21:26:35 +0300

    Add workflow & setup project structure

---
+++ gen.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+SERVICE_NAME=$1
+RELEASE_VERSION=$2
+
+sudo apt-get install -y protobuf-compiler golang-goprotobuf-dev
+
+go install google.golang.org/protobuf/cmd/protoc-gen-go@latest
+go install google.golang.org/grpc/cmd/protoc-gen-go-grpc@latest
+
+protoc --go_out=./golang --go_opt=paths=source_relative \ #B
+--go-grpc_out=./golang --go-grpc_opt=paths=source_relative \
+./${SERVICE_NAME}/*.proto
+
+cd golang/${SERVICE_NAME}
+
+go mod init github.com/krasish/microservices-proto/golang/${SERVICE_NAME} || true
+go mod tidy #D
+
+cd ../../
+
+git config --global user.email "krasishontov@gmail.com"
+git config --global user.name "krasish"
+
+git add . && git commit -am "proto update" || true
+
+git tag -fa golang/${SERVICE_NAME}/${RELEASE_VERSION} \
+-m "golang/${SERVICE_NAME}/${RELEASE_VERSION}"
+
+git push origin refs/tags/golang/${SERVICE_NAME}/${RELEASE_VERSION}
